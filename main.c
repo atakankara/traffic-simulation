beeb0d575ffb10a299fc9e36036036695a96ff07
farrinfedra <fsofian19@ku.edu.tr>
2021-12-22 01:38:06 +0300

    Parsed command line arguments

--- main.c
+++ main.c
@@ -4,8 +4,21 @@
 
 void *print_hello(void *ptr);
 
-int main(){
+int main(int argc, char const *argv[]){
     pthread_t lane_queues[4];
+    // Args: timeLog(arg1) simulationTime(arg2) probability(arg3) seed(arg4)
+
+        //simulationTime
+
+        int time = 0;
+        time = atoi(argv[2]);
+
+        //get probability
+        float prob = 0.0;
+        prob = atof(argv[3]);
+
+        //get seed
+        int seed = 0;
 
     for(int i=0; i<4; i++){
         pthread_create(&lane_queues[i], NULL, print_hello, (void*) i);
